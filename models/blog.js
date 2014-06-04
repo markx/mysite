932ba3039b370588c0c3f5e2a0361053b7d3fcd8
markwithk <markxu18@gmail.com>
2014-06-04 16:23:38 -0400

    add mongoose-auto-increment

--- models/blog.js
+++ models/blog.js
@@ -1,5 +1,8 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var autoIncrement = require('mongoose-auto-increment');
+
+autoIncrement.initialize(mongoose.connection);
 
 var PostSchema = new Schema({
     title : String,
@@ -10,9 +13,11 @@ var PostSchema = new Schema({
 	username: String,
 	createdAt: {type: Date, default : Date.now }
     }]
-});
+},{collection: 'posts'});
 
 PostSchema.path('title').required(true, 'Blog post title cannot be blank');
 PostSchema.path('body').required(true, 'Blog post body cannot be blank');
+
+PostSchema.plugin(autoIncrement.plugin, {model:'Post', field:'id', startAt:1});
 
 mongoose.model('Post', PostSchema);
